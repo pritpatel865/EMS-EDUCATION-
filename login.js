24a134d518cb9775390edb14cadf7e5288de72f6
Hitesh Prajapati <hiteshprajapati4011@gmail.com>
2024-10-25 11:47:12 +0530

    all branch and department added according to the institute

--- login.js
+++ login.js
@@ -97,3 +97,123 @@ document.getElementById("backToLoginLink").addEventListener("click", function(ev
     document.getElementById("login-containerId").classList.remove("hidden");
     document.getElementById("register-containerId").classList.add("hidden");
 });
+
+
+//student update branch option according to the institute
+function stuUpdateBranchOption() {
+    const instituteSelect = document.getElementById("institute");
+    const branchSelect = document.getElementById("stubranch");
+    const selectedInstitute = instituteSelect.value;
+    
+    // Clear the previous branch options
+    branchSelect.innerHTML = "";
+
+    // Populate branch options based on the selected institute
+    if (selectedInstitute === "Institute 1") {
+        branchSelect.innerHTML = `
+            <option value="" disabled selected>Select Branch</option>
+            <option value="comp">Computer Engineering</option>
+            <option value="it">Information Technology</option>
+            <option value="mech">Mechanical Engineering</option>
+            <option value="civil">Civil Engineering</option>
+            <option value="ele">Electrical Engineering</option>
+            <option value="cse">Computer Science and Engineering</option>
+        `;
+    } else if (selectedInstitute === "Institute 2") {
+        branchSelect.innerHTML = `
+            <option value="" disabled selected>Select Branch</option>
+            <option value="bcom">B.Com</option>
+            <option value="mcom">M.Com</option>
+            <option value="bba">BBA</option>
+            <option value="mba">MBA</option>
+        `;
+    } else if (selectedInstitute === "Institute 3") {
+        branchSelect.innerHTML = `
+            <option value="" disabled selected>Select Branch</option>
+            <option value="bsc">B.Sc</option>
+            <option value="msc">M.Sc</option>
+        `;
+    } else if (selectedInstitute === "Institute 4") {
+        branchSelect.innerHTML = `
+            <option value="" disabled selected>Select Branch</option>
+            <option value="bpharm">B.Pharm</option>
+            <option value="mpharm">M.Pharm</option>
+        `;
+    } else {
+        // Default option if no institute is selected
+        branchSelect.innerHTML = `<option value="">Select Branch</option>`;
+    }
+}
+
+//faculty update department option according to the institute
+function facUpdateBranchOption() {
+    const instituteSelect = document.getElementById("facInstitute");
+    const branchSelect = document.getElementById("facDepartment");
+    const selectedInstitute = instituteSelect.value;
+    
+    // Clear the previous branch options
+    branchSelect.innerHTML = "";
+
+    // Populate branch options based on the selected institute
+    if (selectedInstitute === "Institute 1") {
+        branchSelect.innerHTML = `
+            <option value="" disabled selected>Select Department</option>
+            <option value="comp">Computer Engineering</option>
+            <option value="it">Information Technology</option>
+            <option value="mech">Mechanical Engineering</option>
+            <option value="civil">Civil Engineering</option>
+            <option value="ele">Electrical Engineering</option>
+            <option value="cse">Computer Science and Engineering</option>
+        `;
+    } else if (selectedInstitute === "Institute 2") {
+        branchSelect.innerHTML = `
+            <option value="" disabled selected>Select Department</option>
+            <option value="bcom">B.Com</option>
+            <option value="mcom">M.Com</option>
+            <option value="bba">BBA</option>
+            <option value="mba">MBA</option>
+        `;
+    } else if (selectedInstitute === "Institute 3") {
+        branchSelect.innerHTML = `
+            <option value="" disabled selected>Select Department</option>
+            <option value="bsc">B.Sc</option>
+            <option value="msc">M.Sc</option>
+        `;
+    } else if (selectedInstitute === "Institute 4") {
+        branchSelect.innerHTML = `
+            <option value="" disabled selected>Select Department</option>
+            <option value="bpharm">B.Pharm</option>
+            <option value="mpharm">M.Pharm</option>
+        `;
+    } else {
+        // Default option if no institute is selected
+        branchSelect.innerHTML = `<option value="">Select Department</option>`;
+    }
+}
+
+//change login username according to user type
+function updateUsernameField() {
+    const userType = document.getElementById('userType').value;
+    const usernameInput = document.getElementById('username');
+    const usernameLabel = document.getElementById('usernameLabel');
+
+    // Update input type and label based on selected user type
+    switch (userType) {
+        case 'Student':
+            usernameLabel.textContent = 'Enrollment No';
+            usernameInput.type = 'text'; // Setting type to text
+            break;
+        case 'Parent':
+            usernameLabel.textContent = 'Email ID';
+            usernameInput.type = 'email'; // Setting type to email
+            break;
+        case 'Faculty':
+        case 'Admin':
+            usernameLabel.textContent = 'Employee No';
+            usernameInput.type = 'text'; // Setting type to text
+            break;
+        default:
+            usernameLabel.textContent = 'Username';
+            usernameInput.type = 'text';
+    }
+}
